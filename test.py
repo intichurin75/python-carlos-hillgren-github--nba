4670651a875454314cdbb73c002aff9aaf3d6f0c
Carlos Hillgren <carloshillgren@hotmail.com>
2024-02-15 15:27:09 +0100

    Ändrar i koden för att fixa en sak

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ if not(temp >= 0 and temp <= 30):
     print("the temp is good today")
 
 elif not(temp < 0 or temp > 30):
-    print("The temp is bad today, stay inside!")
+    print("The temp is bad today, stay inside or die!")
